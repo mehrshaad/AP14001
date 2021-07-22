034f547b032a65ab5da70d7db5908c3cf9656e31
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2021-07-22 16:22:53 +0430

    updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
     document.getElementById('per').addEventListener('click', function () {
         var a = document.getElementById('head')
         a.classList.add('persian')
-        a.innerHTML = `برنامه سازی پیشرفته
+        a.innerHTML = `برنامه سازی پیشرفته 1400-1401
                 <br>
                 <div class="credits">
                 <h4 class="persian" style="font-size: calc(3px + 0.5vw + 0.5vh);">دانشگاه گیلان 1400</h4>
@@ -12,7 +12,7 @@ window.addEventListener('load', function () {
     document.getElementById('en').addEventListener('click', function () {
         var a = document.getElementById('head')
         a.classList.remove('persian')
-        a.innerHTML = `Advanced Programming
+        a.innerHTML = `Advanced Programming 1400-1401
                 <br>
                 <div class="credits">
                 <h4 style="font-size: calc(3px + 0.5vw + 0.5vh);">© 2021 Copyright Guilan University</h4>

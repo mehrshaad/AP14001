638cf3910c6f17feda26f4f8f22f32c1af3f0561
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2021-08-04 20:09:50 +0430

    removed loading animation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 isPersian = true;
 
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector("#loader").style.visibility = "visible";
-    } else {
-        document.querySelector("#loader").style.display = "none";
-        document.querySelector("body").style.visibility = "visible";
-    }
-};
+// document.onreadystatechange = function () {
+//     if (document.readyState !== "complete") {
+//         document.querySelector("body").style.visibility = "hidden";
+//         document.querySelector("#loader").style.visibility = "visible";
+//     } else {
+//         document.querySelector("#loader").style.display = "none";
+//         document.querySelector("body").style.visibility = "visible";
+//     }
+// };
 
 function translateToPersian() {
     var a = document.getElementById('per')
